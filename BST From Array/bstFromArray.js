cfcc4947230d76e7bcf72fa7dc76e3bb77e4ac79
Steven O'Neill <oneillsteven957@gmail.com>
2019-05-09 17:38:23 -0500

    started BSTFromArray problem

---
+++ BST From Array/bstFromArray.js
@@ -0,0 +1,16 @@
+function createMinBst(sortedArray) {
+    return createMinBst(sortedArray, 0, sortedArray.length - 1);
+}
+
+function createMinBstHelper(sortedArray, left, right) {
+    if (right < left) return null;
+
+    const mid = math.floor((left + right) / 2);
+    const node = new BinaryTreeNode(sortedArray);
+
+    node.left = createMinBstHelper(sortedArray, left, mid - 1);
+    node.right = createMinBstHelper(sortedArray, mid + 1, right);
+
+    return node;
+}
+
