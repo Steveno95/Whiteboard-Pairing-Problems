5fb4768109dbac2daaed0711b07738271b36a193
Steven O'Neill <oneillsteven957@gmail.com>
2019-05-10 15:33:33 -0500

    class for binary tree node

--- Balanced Binary Tree/balancedBinaryTree.py
+++ Balanced Binary Tree/balancedBinaryTree.py
@@ -13,3 +13,17 @@ def checkBalance(root):
         return 1 + max(maxDepth(node.left), maxDepth(node.right))
 
     return maxDepth(root) - minDepth(root) == 0
+
+class BinaryTreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insertLeft(self, value):
+        self.left = BinaryTreeNode(value)
+        return self.left
+
+    def insertRight(self, value):
+        self.right = BinaryTreeNode(value)
+        return self.right
