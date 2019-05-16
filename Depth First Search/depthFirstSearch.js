05c81ec8f9aed09ed274a76f8a08e04ffb2c7e6f
Steven O'Neill <oneillsteven957@gmail.com>
2019-05-16 16:16:45 -0500

    added tests for dfs functions

--- Depth First Search/depthFirstSearch.js
+++ Depth First Search/depthFirstSearch.js
@@ -40,9 +40,31 @@ class BinaryTreeNode {
         this.left = new BinaryTreeNode(value);
         return this.left;
     }
-    
+
     insertRight(value) {
         this.right = new BinaryTreeNode(value);
         return this.right;
     }
 }
+
+
+const root = new BinaryTreeNode(6);
+root.insertLeft(10);
+root.insertRight(18);
+root.left.insertLeft(9);
+root.right.insertRight(89);
+
+const cb = (x) => console.log(x);
+
+rdfs(root, cb);  // should print 6 10 9 18 89
+console.log();
+idfs(root, cb);  // should print 6 10 9 18 89
+console.log();
+
+root.left.insertRight(15);
+root.right.insertLeft(0);
+
+rdfs(root, cb);  // should print 6 10 9 15 18 0 89 
+console.log();
+idfs(root, cb);  // should print 6 10 9 15 18 0 89  
+console.log();
