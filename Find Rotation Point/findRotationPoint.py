fba9e53efaae53e80cffa5ec78d0be2b9791a971
Steven O'Neill <oneillsteven957@gmail.com>
2019-05-16 16:36:56 -0500

    finished function for finding rotation point

--- Find Rotation Point/findRotationPoint.py
+++ Find Rotation Point/findRotationPoint.py
@@ -1,4 +1,19 @@
 import math
 
 def findRotationPoint(words):
+    firstWord = words[0]
+    floorIndex = 0
+    ceilingIndex = len(words) - 1
+
+    while floorIndex < ceilingIndex:
+        guessIndex = math.floor(floorIndex + ((ceilingIndex - floorIndex) / 2))
+
+        if words[guessIndex] >= firstWord:
+            floorIndex = guessIndex
+        else:
+            ceilingIndex = guessIndex
+
+        if floorIndex + 1 == ceilingIndex:
+            break
     
+    return ceilingIndex
