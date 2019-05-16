0b27974f3376a93e6e5927e9c05e562a53b5c30c
Steven O'Neill <oneillsteven957@gmail.com>
2019-05-16 16:09:13 -0500

    iterative dfs function

--- Depth First Search/depthFirstSearch.js
+++ Depth First Search/depthFirstSearch.js
@@ -9,3 +9,22 @@ function rdfs(node, cb) {
         rdfs(node.right, cb);
     }
 }
+
+function idfs(node, cb) {
+    const stack = [];
+    stack.push(node);
+
+    while (stack.length > 0) {
+        const current = stack.pop();
+
+        if (current.right) {
+            stack.push(current.right);
+        }
+
+        if (current.left) {
+            stack.push(current.left);
+        }
+
+        cb(current.value);
+    }
+}
