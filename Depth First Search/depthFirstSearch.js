13f602851e9341c8725a182b649b40e8eb4fdbdf
Steven O'Neill <oneillsteven957@gmail.com>
2019-05-16 16:12:57 -0500

    Binary tree node class

--- Depth First Search/depthFirstSearch.js
+++ Depth First Search/depthFirstSearch.js
@@ -28,3 +28,21 @@ function idfs(node, cb) {
         cb(current.value);
     }
 }
+
+class BinaryTreeNode {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+
+    insertLeft(value) {
+        this.left = new BinaryTreeNode(value);
+        return this.left;
+    }
+    
+    insertRight(value) {
+        this.right = new BinaryTreeNode(value);
+        return this.right;
+    }
+}
