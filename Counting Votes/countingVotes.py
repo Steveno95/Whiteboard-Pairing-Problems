458d243562e6c48a3f2d32272f8f44d302dc3bb0
Steven O'Neill <oneillsteven957@gmail.com>
2019-05-20 14:15:57 -0500

    added problem description for counting votes

--- Counting Votes/countingVotes.py
+++ Counting Votes/countingVotes.py
@@ -1,3 +1,9 @@
+# You've been hired to write the software to count the votes for a local election. 
+# Write a function countVotes that receives an array of (unique) names, each one representing a vote for that person. 
+# Your function should return the name of the winner of the election. 
+# In the case of a tie, the person whose name comes last alphabetically wins the election (a dumb rule to be sure, but the votes don't need to know).
+
+
 def count_votes(arr):
     counts = {}
     max_votes = 0
