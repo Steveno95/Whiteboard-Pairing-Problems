ab865cf50b795ae06bedcc8010716c0a1ae52b29
Steven O'Neill <oneillsteven957@gmail.com>
2019-05-20 14:22:34 -0500

    added problem description for integer pairs

--- Integer Pairs/integerPairs.js
+++ Integer Pairs/integerPairs.js
@@ -1,3 +1,13 @@
+// Write a function integerPairs to find and print out all pairs of integers within an input array which sum up to a specified input value k.
+
+// There are multiple ways to do this, depending upon whether you want to favor runtime or space.
+
+// Example:
+
+// integerPairs([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 11);
+// should print '6 5', '7 4', '8 3', '9 2', '10 1'
+
+
 function integerPairs(arr, k) {
     const hash = {};
     arr.forEach((x, i) => {
