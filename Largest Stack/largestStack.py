d72ae6647ca5a127d87b93e02ab4bc4a56ca3755
Steven O'Neill <oneillsteven957@gmail.com>
2019-05-28 13:56:11 -0500

    problem description

---
+++ Largest Stack/largestStack.py
@@ -0,0 +1,29 @@
+# Assume we have a Stack class given by the following code:
+
+# class Stack {
+#   constructor() {
+#     this.items = [];
+#   }
+
+#   push(item) {
+#     this.items.push(item);
+#   }
+
+#   pop() {
+#     if (this.items.length) {
+#       return this.items.pop();
+#     }
+#     return null;
+#   }
+
+#   peek() {
+#     if (this.items.length) {
+#       return this.items[this.items.length-1];
+#     }
+#     return null;
+#   }
+# }
+# We wish to augment this Stack class such that we can always fetch the max value from the Stack in constant time.
+
+# Use the given Stack class to implement a new MaxStack class that has all the same methods as the base Stack class, 
+# along with a getMax method that returns the max value of the MaxStack in O(1) time. getMax should not remove the item.
