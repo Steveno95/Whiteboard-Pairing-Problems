906e1a64cbd6ec7388052677d262442ba9a368ba
Steven O'Neill <oneillsteven957@gmail.com>
2019-06-28 13:09:35 -0500

    added problem description

---
+++ Reversed Link List/reversedLinkList.js
@@ -0,0 +1,14 @@
+// Reverse a Linked List
+// Write a function reverseLinkedList that receives a linked list node as input and then reverses the list, starting from the given node. Do this in-place without using any extra memory. Your function should return the value of the new head of the list.
+
+// Here is a simple Linked List class:
+
+// JavaScript:
+
+// class ListNode {
+//   constructor(value) {
+//     this.value = value;
+//     this.next = null;
+//   }
+// }
+
