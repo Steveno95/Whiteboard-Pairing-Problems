444a2154677e7061ca769caca57365d5a8cca8f3
Steven O'Neill <oneillsteven957@gmail.com>
2019-07-02 14:30:12 -0500

    added nodes to the linked list

--- Reversed Link List/reversedLinkList.js
+++ Reversed Link List/reversedLinkList.js
@@ -34,3 +34,15 @@ class ListNode {
   }
 }
 
+const a = new ListNode('a');
+const b = new ListNode('b');
+const c = new ListNode('c');
+const d = new ListNode('d');
+const e = new ListNode('e');
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+
+
