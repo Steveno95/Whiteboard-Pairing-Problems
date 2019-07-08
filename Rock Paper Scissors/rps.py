e19e477de7902f9f8a0f98352ded0dbc7f73a55f
Steven O'Neill <oneillsteven957@gmail.com>
2019-07-08 17:25:21 -0500

    created a while loop

--- Rock Paper Scissors/rps.py
+++ Rock Paper Scissors/rps.py
@@ -11,4 +11,15 @@ def rps(n):
     if n == 1:
         return base
     
+    count = 1
+    while count != n:
+        for inner_list in prev_list:
+            for base_item in base:
+                temp_inner_list = inner_list.copy()
+                temp_inner_list.append(base_item[0])
+                result.append(temp_inner_list)
+
+        count += 1
+        prev_list = result.copy()
+        result = []
     
