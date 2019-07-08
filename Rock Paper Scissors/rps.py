10fd90cebda76edd9c651fe31c734f409fceadcd
Steven O'Neill <oneillsteven957@gmail.com>
2019-07-08 17:22:10 -0500

    added some if statements

--- Rock Paper Scissors/rps.py
+++ Rock Paper Scissors/rps.py
@@ -4,3 +4,11 @@ def rps(n):
     base = [['rock'], ['paper'], ['scissors']]
     prev_list = [['rock'], ['paper'], ['scissors']]
     result = []
+
+    if n == 0:
+        return [[]]
+
+    if n == 1:
+        return base
+    
+    
