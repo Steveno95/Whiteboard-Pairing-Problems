5ae50bfcba46efdc8ef21ab01ca3a7e07c329a55
Steven O'Neill <oneillsteven957@gmail.com>
2019-07-11 14:48:52 -0500

    added console log tests

--- Rotate Image/rotateImage.js
+++ Rotate Image/rotateImage.js
@@ -41,3 +41,24 @@ function rotateImage(matrix) {
     }
     return matrix;
 }
+
+
+console.log(rotateImage([
+    [1, 2],
+    [3, 4]
+]));
+// should print [ [2, 4], [1, 3] ]
+console.log('-------------------');
+console.log(rotateImage([
+    [1, 1, 5, 9, 9],
+    [2, 2, 6, 0, 0],
+    [3, 3, 7, 1, 1],
+    [4, 4, 8, 2, 2],
+    [5, 5, 9, 3, 3]
+]));
+// should print
+// [ [ 9, 0, 1, 2, 3 ],
+//   [ 9, 0, 1, 2, 3 ],
+//   [ 5, 6, 7, 8, 9 ],
+//   [ 1, 2, 3, 4, 5 ],
+//   [ 1, 2, 3, 4, 5 ] ]
