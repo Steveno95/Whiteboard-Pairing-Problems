9782127d595e320f7d2531f67813f7e68b1bd7bd
Steven O'Neill <oneillsteven957@gmail.com>
2019-07-12 13:55:45 -0500

    added tests and fixed code

--- Search In Sorted Matrix/searchInSortedMatrix.js
+++ Search In Sorted Matrix/searchInSortedMatrix.js
@@ -15,18 +15,39 @@
 // Expected output: [3, 4]
 
 function searchSortedMatrix(matrix, target) {
-    let row = 0;
-    let col = matrix[0].length - 1;
+    var m = matrix.length,
+        n = matrix[0].length,
+        row = 0,
+        col = n - 1;
 
-    while (row < matrix.length && col >= 0) {
-        if (matrix[col][row] > target) {
-            col --;
-        } else if (matrix[col][row] < target) {
-            row ++;
+    if (target < matrix[0][0] || target > matrix[m - 1][n - 1]) {
+        return [-1, -1];
+    }
+
+    while (row <= (n - 1) && col >= 0) {
+        if (matrix[row][col] < target) {
+            row++;
+        } else if (matrix[row][col] > target) {
+            col--;
         } else {
             return [row, col];
         }
     }
-
     return [-1, -1];
 }
+
+
+const matrix = [ 
+    [1, 4, 7, 12, 15, 999], 
+    [2, 5, 19, 32, 35, 1001], 
+    [4, 8, 24, 34, 36, 1002], 
+    [40, 41, 42, 44, 45, 1003], 
+    [98, 99, 101, 104, 190, 1009], 
+];
+
+console.log(searchSortedMatrix(matrix, 1));    // should print [0, 0]
+console.log(searchSortedMatrix(matrix, 7));    // should print [0, 2]
+console.log(searchSortedMatrix(matrix, 999));  // should print [0, 5]
+console.log(searchSortedMatrix(matrix, 1001)); // should print [1, 5]
+console.log(searchSortedMatrix(matrix, 104));  // should print [4, 3]
+console.log(searchSortedMatrix(matrix, 1010)); // should print [-1, -1]
