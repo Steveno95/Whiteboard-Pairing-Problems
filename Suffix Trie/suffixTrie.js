54e7d06572f78b472e5b419e3882337438a839c1
Steven O'Neill <oneillsteven957@gmail.com>
2019-07-23 18:11:00 -0500

    added problem description

---
+++ Suffix Trie/suffixTrie.js
@@ -0,0 +1,29 @@
+// Write a class for a such a suffix trie data structure. This class should have a root property representing the root node of the trie. 
+// It should support creation from an initial string passed to the constructor as well as searching of strings.
+
+// The creation method will be called when the class is instantiated and should populate the root node with a subtries containing all of the suffixes of the given string. 
+// Note that every string added to the trie should end with the "endSymbol" character "*".
+
+// trie = SuffixTrie("pogo")
+// print(trie.root)  # should print the following
+// {
+//     "p": {
+//         "o": {
+//             "g": {
+//                 "o": {'*': True}
+//             }
+//         }
+//     }, 
+//     "o": {
+//         "g": {
+//             "o": {'*': True}
+//         }, 
+//         "*": True}, 
+//     "g": {
+//         "o": {'*': True}
+//     }
+// }
+
+// print(trie.contains("ogo"))  # should print True
+
+
