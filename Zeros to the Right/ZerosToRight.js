ac963c684270357cd979b2cccf8f37e3c388f6a4
Steven O'Neill <oneillsteven957@gmail.com>
2019-09-11 14:52:26 -0500

    added tests

--- Zeros to the Right/ZerosToRight.js
+++ Zeros to the Right/ZerosToRight.js
@@ -35,3 +35,33 @@ function zerosToTheRight(arr) {
     console.log(arr);
     return arr.length - nonZeros;
 }
+
+console.log('Number of non-zero ints: ', zerosToTheRight([0, 3, 1, 0, -2]));
+console.log('');
+// This should print:
+// [-2, 3, 1, 0, 0]
+// Numbers of non-zero ints: 3
+
+console.log('Number of non-zero ints: ', zerosToTheRight([1, 2, 3, 0, 4, 0, 0]));
+console.log('');
+// This should print:
+// [1, 2, 3, 4, 0, 0, 0]
+// Numbers of non-zero ints: 4
+
+console.log('Number of non-zero ints: ', zerosToTheRight([4, 1, 2, 5]));
+console.log('');
+// This should print:
+// [4, 1, 2, 5]
+// Numbers of non-zero ints: 4
+
+console.log('Number of non-zero ints: ', zerosToTheRight([0, 0, 0, 0, 0]));
+console.log('');
+// This should print:
+// [0, 0, 0, 0, 0]
+// Numbers of non-zero ints: 0
+
+console.log('Number of non-zero ints: ', zerosToTheRight([0, 0, 0, 0, 3, 2, 1]));
+console.log('');
+// This should print:
+// [1, 2, 3, 0, 0, 0, 0]
+// Numbers of non-zero ints: 3
